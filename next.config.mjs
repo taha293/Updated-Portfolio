a65a5b9f6030712c1055096dec87e809008d08bf
Muhammad Taha <tahahero7890@gmail.com>
2025-04-29 22:39:45 +0500

    type fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,6 @@ const nextConfig = {
           "api.microlink.io", // Microlink Image Preview
         ],
       },
-      "extends": [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended" // Or your other recommended TypeScript rules
-  ],
-  "rules": {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unsafe-function-type": "off"
-    // Add other rule overrides here if needed
-  }
 };
 
 export default nextConfig;
