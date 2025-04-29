1f20fe24e55933de9b7ab00f339f1bb6fee66590
Muhammad Taha <tahahero7890@gmail.com>
2025-04-29 22:34:31 +0500

    type error solved

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
           "api.microlink.io", // Microlink Image Preview
         ],
       },
+      "scripts": {
+  "dev": "next dev",
+  "build": "npm run build:vercel",
+  "build:local": "next build",
+  "build:vercel": "next build",
+  "start": "next start",
+  "lint": "next lint"
+}
 };
 
 export default nextConfig;
